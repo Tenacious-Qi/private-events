77e9ca3eef5ed8f6d9f59d4bbee6a072bdd41182
Tenacious-Qi <joe.mccann@mailbox.org>
2021-04-17 03:14:04 -0400

    improve UI. Finish styling events#show page

--- app/models/event.rb
+++ app/models/event.rb
@@ -11,14 +11,16 @@ class Event < ApplicationRecord
 
   validates :title,  presence: true, length: { in: 3..100 }, uniqueness: true;
   validates :location, presence: true, length: { in: 3..30 }
+  validates :description, length: { in: 10..500}, allow_blank: true
   validates :start_time, presence: true
   validate  :start_time_in_future
 
   private
     def start_time_in_future
-      if start_time < Time.zone.now
-        errors.add(:start_time, "must be in future")
+      if start_time
+        if start_time < Time.zone.now
+          errors.add(:start_time, "must be in future")
+        end
       end
     end
-
 end
