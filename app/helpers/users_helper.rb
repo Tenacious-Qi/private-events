659f62ff1a20841063a63800e45ca4a8d2b3045e
Tenacious-Qi <joe.mccann@mailbox.org>
2021-04-19 00:32:10 -0400

    complete majority of styling

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -7,4 +7,12 @@ module UsersHelper
     def previously_attended_events
       @user.invited_events.past.includes(:invitations).where("attending = 'yes'")      
     end
+
+    def events_planning_to_attend
+      @user.invited_events.upcoming.includes(:invitations).where("attending = 'yes'")
+    end
+
+    def events_with_pending_rsvp
+      @user.invited_events.upcoming.includes(:invitations).where("attending = 'no response'")
+    end
 end
