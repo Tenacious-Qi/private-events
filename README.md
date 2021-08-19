5730ef5b02e7cd732d48f7ce4d448a6807c2f5f0
Tenacious-Qi <joe.mccann@mailbox.org>
2021-08-19 19:28:37 -0400

    Update README.md. Seed database.

--- README.md
+++ README.md
@@ -2,6 +2,36 @@
 
 This is the repository for a Rails app similar to Eventbrite. It is a project in Active Record Associations as part of The Odin Project [curriculum](https://www.theodinproject.com/paths/full-stack-ruby-on-rails/courses/ruby-on-rails/lessons/associations).
 
+### Installation
+
+- #### Step 1: Install Locally
+
+  1. clone this repo
+  2. cd into cloned directory `cd private-events`
+  3. run `bundle install`
+
+- #### Step 2: Seed the database
+
+  1. `rails db:migrate`
+  2. `rails db:seed`
+
+- #### Step 3: Fire up Rails Server
+
+  1. `rails server`
+  2. Navigate to [localhost:3000](localhost:3000) in your browser
+
+### Running the Tests
+
+#### To run all the tests
+
+- `rspec spec/` from the `private-events` directory.
+
+#### To run specific spec categories
+
+- `rspec spec/features`
+- `rspec spec/models`
+- `rspec spec/requests`
+
 ### Authentication and Login
 
 - uses `bcrypt` and `has_secure_password` for authentication
@@ -17,12 +47,6 @@ This is the repository for a Rails app similar to Eventbrite. It is a project in
 - use `scope` to limit records returned to a specific constraint
 - use `includes` method to eager load records where appropriate
 - use `joins` to join two tables together when necessary
-
-### Testing
-
-- RSpec and Capybara
-- Request, Model, and Feature specs
-
 
 #### Notable Gems used
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,38 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+users = []
+events = []
+
+# default login for people to try site
+users << User.create(name: 'Foo Bar',
+                     email: 'foo@bar.com',
+                     password: 'foobar'
+)
+
+12.times do |n|
+  users << User.create(name: Faker::Name.name, 
+                       email: Faker::Internet.unique.email, 
+                       password: Faker::Internet.password
+                      )
+end
+
+20.times do |n|
+  events << Event.create(host: users.sample, 
+                         title: Faker::Lorem.sentence(word_count: 4, random_words_to_add: 2),
+                         location: Faker::Address.city,
+                         description: Faker::Lorem.paragraph(sentence_count: 12),
+                         start_time: Faker::Time.between(from: Time.zone.now + 2.days, to: Time.zone.now + 10.days)
+                        )
+end
+
+# invite random sample of users to each event
+events.each_with_index do |event, event_index|
+  # sample a random user as host
+  host = users.sample
+  # e.g., [user_1, user_5, user_12, user7]
+  # don't invite host to their own event
+  users_sample = users.sample(4).reject { |u| u == host }
+  users_sample.each_with_index do |user, user_index|
+    invitation = Invitation.create(host: host, invitee: user, event: event)
+    # semi-random assignment of rsvps
+    invitation.update_attribute(:attending, 'yes') if event_index.even? && user_index.odd?
+  end
+end
