3b18f809ec0b42a8a432577a8a635fe39518b80d
Tenacious-Qi <joe.mccann@mailbox.org>
2021-08-19 15:30:08 -0400

    Route 'get /users' to 'users#new' upon page refresh after a failed user signup.'

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   get    '/signup', to: 'users#new'
   get    '/login',  to: 'sessions#new'
+  # necessary to prevent error on page refresh after a failed signup
+  get    '/users',  to: 'users#new'
   post   '/login',  to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
   resources :users, only: [:new, :create, :show]
