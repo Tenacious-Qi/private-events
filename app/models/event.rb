19bfac09599c82001737d7f01590dc0fe451c43b
Tenacious-Qi <joe.mccann@mailbox.org>
2021-08-23 02:33:19 -0400

    Add 'dependent: :destroy' to Event model.

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,7 +1,8 @@
 class Event < ApplicationRecord
   belongs_to :host, class_name: "User"
 
-  has_many :invitations, foreign_key: "event_id"
+  has_many :invitations, foreign_key: "event_id",
+                         dependent: :destroy
 
   has_many :invitees, through: :invitations,
                       class_name: "User"
