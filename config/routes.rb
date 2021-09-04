dbe6c6fdafcd94385bbbf391b3223f89ae9067db
Tenacious-Qi <joe.mccann@mailbox.org>
2021-09-03 23:42:27 -0400

    Add a redirect for get '/invitations'.

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   resources :users
   resources :events
   resources :invitations, only: [:destroy, :new, :create, :update]
+  get '/invitations', to: redirect('/events')
 
   root 'events#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
