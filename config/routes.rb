f64694d98755ee639f390216a03a452a7c3f0dca
Tenacious-Qi <joe.mccann@mailbox.org>
2021-10-14 15:02:05 -0400

    Setup message resources.

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
   resources :users
   resources :events
   resources :invitations, only: [:destroy, :new, :create, :update]
+  resources :messages, only: [:create]
   get '/invitations', to: redirect('/events')
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_29_013500) do
+ActiveRecord::Schema.define(version: 2021_10_14_185726) do
 
   create_table "events", force: :cascade do |t|
     t.string "location"
@@ -32,6 +32,14 @@ ActiveRecord::Schema.define(version: 2021_04_29_013500) do
     t.index ["invitee_id", "event_id"], name: "index_invitations_on_invitee_id_and_event_id", unique: true
   end
 
+  create_table "messages", force: :cascade do |t|
+    t.text "body"
+    t.integer "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_messages_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
@@ -39,7 +47,11 @@ ActiveRecord::Schema.define(version: 2021_04_29_013500) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "auth_token"
+    t.boolean "online", default: false
+    t.boolean "away", default: true
+    t.string "current_room"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "messages", "users"
 end
