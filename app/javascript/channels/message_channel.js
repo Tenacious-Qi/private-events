d075716c4ea61df2c2f304a2d4d5197c7fe7c3f4
Tenacious-Qi <joe.mccann@mailbox.org>
2021-10-15 17:22:29 -0400

    Show time message was sent, scroll to most recent comment.

--- app/javascript/channels/message_channel.js
+++ app/javascript/channels/message_channel.js
@@ -13,12 +13,15 @@ const messageChannel = consumer.subscriptions.create("MessageChannel", {
     // Called when there's incoming data on the websocket for this channel
     const messageDisplay = document.querySelector('#message-display')
     messageDisplay.insertAdjacentHTML('beforeend', this.template(data))
+    autoScroll(messageDisplay)
   },
 
   template(data) {
-    return `<article class="message">
+    const timeOfComment = utcFormattedTime();
+    return `<article class="message is-info">
               <div class="message-header">
                 <p>${data.user.name}</p>
+                <p>${timeOfComment}</p>
               </div>
               <div class="message-body">
                 <p>${data.body}</p>
@@ -27,3 +30,13 @@ const messageChannel = consumer.subscriptions.create("MessageChannel", {
   }
 });
 
+const autoScroll = (messageDisplay) => {
+  messageDisplay.scrollTop = messageDisplay.scrollHeight
+}
+
+const utcFormattedTime = () => {
+  const today = new Date();
+  const date = today.getUTCFullYear()+'-'+(today.getUTCMonth()+1)+'-'+today.getUTCDate();
+  const time = today.getUTCHours() + ":" + today.getUTCMinutes() + ":" + today.getUTCSeconds();
+  return `${date} ${time} UTC`
+}
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,6 @@
 class Message < ApplicationRecord
   belongs_to :user
   belongs_to :event
+
+  validates :body, length: { maximum: 500 }
 end
