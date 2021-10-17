06e5f793404b0784602623f92a0a4dedd533ecd1
Tenacious-Qi <joe.mccann@mailbox.org>
2021-10-17 02:15:19 -0400

    Setup redis for WebSockets.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -56,8 +56,8 @@ Rails.application.configure do
 
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
-  # config.action_cable.url = 'wss://example.com/cable'
-  # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
+  config.action_cable.url = 'wss://fast-citadel-83141.herokuapp.com/cable'
+  config.action_cable.allowed_request_origins = [ 'http://fast-citadel-83141.herokuapp.com', /http:\/\/fast-citadel-83141.herokuapp.com.*/ ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
