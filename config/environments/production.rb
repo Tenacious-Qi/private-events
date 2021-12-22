a2e77c10ff4e27dd27e077eac126ad11626730ec
Tenacious-Qi <joe.mccann@mailbox.org>
2021-12-22 02:56:15 -0500

    Use https for all requests.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,6 +3,8 @@ require "active_support/core_ext/integer/time"
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+    
+
     # default host for production
     host = 'fast-citadel-83141.herokuapp.com'
     config.action_mailer.default_url_options = { host: host }
