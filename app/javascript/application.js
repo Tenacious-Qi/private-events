59735e53469b0e8771d102dc94b8f7139261055a
Joseph McCann <joe.mccann@mailbox.org>
2022-08-09 17:37:56 -0400

    Install importmaps so that turbo:load event listener works for hamburger menu.

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,1 +1,12 @@
-import "./controllers"
+// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+import "@hotwired/turbo-rails"
+import "controllers"
+
+
+document.addEventListener("turbo:load", () => {
+  const burgerIcon = document.querySelector('#burger');
+  const navbarMenu = document.querySelector('#nav-links');
+  burgerIcon.addEventListener('click', () => {
+    navbarMenu.classList.toggle('is-active')
+  })
+});
